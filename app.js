e411e44f891cb625342d94ae58ec00d6d55b65c9
ivancevic <ivancevic.1974@gmail.com>
2025-01-16 13:22:42 +0100

    Add 0.0.0.0

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post('/submit', async (req, res) => {
 
 // Start Server
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
